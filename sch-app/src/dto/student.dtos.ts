e55e91fc9d28cf22e525b35467d68ad8fa141fa5
Taofeek <ibrahimtaofeek005@gmail.com>
2024-11-26 21:11:38 +0100

    interfaces, dtos & schemas added

--- sch-app/src/dto/student.dtos.ts
+++ sch-app/src/dto/student.dtos.ts
@@ -6,16 +6,20 @@ export class CreateStudentDto {
     @MaxLength(30)
     @IsNotEmpty()
     readonly name: string;
+
     @IsNumber()
     @IsNotEmpty()
     readonly roleNumber: number;
+
     @IsString()
     @MaxLength(30)
     @IsNotEmpty()
     readonly gender: string;
+
     @IsNumber()
     @IsNotEmpty()
     readonly marks: number;
+    
     @IsNumber()
     @IsNotEmpty()
     readonly class: number;
