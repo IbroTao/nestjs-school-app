ecf086a4010cecb65ab9b4d0efac6a027a0f66bf
Taofeek <ibrahimtaofeek005@gmail.com>
2024-12-18 11:50:31 +0100

    changed the file module name to default

--- sch-app/src/main.ts
+++ sch-app/src/main.ts
@@ -3,9 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common'
 
-async function bootstrap() {
+async function createApp() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
-bootstrap();
+createApp();
